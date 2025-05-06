c607bc4e0c43210f9364ca2c5dad5d1f69aca6d2
qf-travis-terry <98347356+qf-travis-terry@users.noreply.github.com>
2025-05-06 11:22:59 -0400

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/kris-yard-revamp/", // 👈 critical for GitHub Pages!
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
